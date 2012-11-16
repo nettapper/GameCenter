2ba8c97332f4c64966c5c4cf1a68b7430f21dfb8
Conner D <connerdunn7399@gmail.com>
2012-11-15 17:03:16 -0700

    merged newClient into ClientControl

--- GameCenterClient/src/client/ClientControl.java
+++ GameCenterClient/src/client/ClientControl.java
@@ -1,6 +1,9 @@
 package client;
 
-import client.NewClient;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class ClientControl {
 	protected static final String ipaddress = "127.0.0.1";
@@ -10,10 +13,45 @@ public class ClientControl {
 	protected static String data = "lol lol lol im better than you!";
 	
 	public static void main(String[] args) {
-		NewClient client = new NewClient();
 		while(indexNum < path.length){
-			client.connect("http://"+ipaddress+":"+port+"/"+path[indexNum], data);
+			connect("http://"+ipaddress+":"+port+"/"+path[indexNum], data);
 			indexNum++;
 		}
 	}
+	//New Client Below
+
+	public static String connect(String address, String outputData) {
+		System.out.println("Client attempting a connection to \""+address+".\"");
+		
+		HttpURLConnection connection;
+		URL link;
+		InputStream input;
+		String data = "";
+		DataOutputStream output;
+		
+		try {
+		    link = new URL(address);
+		    connection = (HttpURLConnection) link.openConnection();
+		    connection.setAllowUserInteraction(true);
+		    connection.setRequestMethod("POST");
+		    connection.setDoOutput(true);
+		    connection.setDoInput(true);
+		    connection.setInstanceFollowRedirects(false);
+		    
+		    output = new DataOutputStream(connection.getOutputStream());
+		    output.writeBytes(outputData);
+		    output.flush();
+		    output.close();
+		    
+		    input = connection.getInputStream();
+		    while (input.available() > 0) {
+			int number = input.read();
+			data += Character.toString((char) number);
+		    }
+		    connection.disconnect();
+		    input.close();
+		    System.out.println("Client succesfully connected to \""+address+".\" and read \""+data+".\"");
+		} catch (Exception Error) {Error.printStackTrace();}
+		return data;
+	    }
 }
